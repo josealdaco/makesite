9271f5b4329f1c876cbe45c0429fb1c5293f3997
siko408 <gamerbloxhd@gmail.com>
2020-06-08 02:28:13 -0700

    3 steps completed

--- makesite.go
+++ makesite.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"io/ioutil";
-	"fmt"
+	"fmt";
+	"html/template";
+        "os";
 )
+type Entry struct {
+        Content string
+
+}
+
+
 func readFile() string {
 	fileContents, err := ioutil.ReadFile("first-post.txt")
 	if err != nil {
@@ -16,6 +24,16 @@ func main() {
 	fmt.Println("Hello, world!")
 	read:=readFile()
 	fmt.Println(read)
+	paths := []string{
+		 "template.tmpl",
+	   }
+	 td := Entry{read}
+
+	 t := template.Must(template.New("template.tmpl").ParseFiles(paths...))
+         var err = t.Execute(os.Stdout, td)
+         if err != nil {
+           panic(err)
+         }
 
 }
 
